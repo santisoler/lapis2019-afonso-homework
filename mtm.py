9121c76227ce591ebfc2a44592e9f44bea2b7fb8
Santiago Soler <santiago.r.soler@gmail.com>
2019-06-12 12:52:38 -0300

    Add mtm.py

---
+++ mtm.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+LAM = 12
+
+
+def target_pdf(x1, x2):
+    """
+    Compute target pdf
+
+    Parameters
+    ----------
+    x1: array
+        Horizontal coordinates of points where the PDF will be computed.
+    x2: array
+        Vertical coordinates of points where the PDF will be computed.
+
+    Returns
+    -------
+    target_pdf : array
+        Target pdf on each point.
+    """
+    return np.exp(-(x1 ** 2 + x2 ** 2 + (x1 * x2) ** 2) - 2 * LAM * x1 * x2 / 2)
+
+
+def likelihood(x):
+    """
+    Likelihood of x
+
+    Parameters
+    ----------
+    x : 1d-array or 2d-array
+        Array containing the horizontal and vertical component of one or several
+        vectors.
+
+    Returns
+    -------
+    likelihood : float or 1d-array
+        Likelihood of vector x.
+    """
+    # Convert the argument into 2d-array in order to make this function capable of
+    # computing likelihoods for multiple vectors
+    x = np.atleast_2d(x)
+    x1 = x[:, 0]
+    x2 = x[:, 1]
+    return np.exp(-(x1 ** 2 + x2 ** 2 + (x1 * x2) ** 2) - 2 * LAM * x1 * x2 / 2)
+
+
+# Define parameters like number of iterations, number of trials, sigma for the T(x, y)
+# distribution
+iterations = int(10e3)
+n_trials = 20
+sigma = 3
+
+# Run MTM
+# -------
+data = []
+x = np.array([0, 0])
+for i in range(iterations):
+    # Get y_trials around x with gaussian probability
+    y_trials = x + sigma * np.random.randn(n_trials, 2)
+    # Compute probability of each element of y_trials
+    probabilities_y = likelihood(y_trials)
+    # Select y among the y_trials with probability of pi(y_trials)
+    # I have to normalize the probabilities in order to sum one
+    index = np.random.choice(n_trials, p=probabilities_y / probabilities_y.sum())
+    y = y_trials[index]
+
+    # Generate the reference set (assigning the last element to the current x)
+    reference_set = y + sigma * np.random.randn(n_trials, 2)
+    reference_set[-1] = x
+    # Compute the generalized M-H ratio
+    rg = min(1, probabilities_y.sum() / likelihood(reference_set).sum())
+    # Lets accept y with probability rg
+    if rg > np.random.rand():
+        data.append(y)
+        x = y
+
+
+# Plot results and target PDF
+data = np.array(data)
+x1 = np.linspace(-4, 4, 101)
+x1, x2 = np.meshgrid(x1, x1)
+target = target_pdf(x1, x2)
+
+plt.contour(x1, x2, target)
+plt.scatter(data[:, 0], data[:, 1], s=1, label="Accepted points by MTM")
+plt.axes().set_aspect("equal")
+plt.grid()
+plt.legend()
+plt.show()
